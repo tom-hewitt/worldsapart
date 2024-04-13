7ad49b76805446d761fc5b1d5d3e1e4dc0f63aa5
z07151th <thomas.hewitt-3@student.manchester.ac.uk>
2024-04-13 21:49:18 +0100

    smooth planet

--- components/planet/Planet.tsx
+++ components/planet/Planet.tsx
@@ -1,35 +1,27 @@
 import * as THREE from "three";
 import React, { useRef } from "react";
-import { ThreeElements } from "@react-three/fiber";
 
-interface PlanetProps {
-    name?: string;
-    radius?: number;
-    segments?: number;
-    color?: string;
-    rotation?: [number, number, number];
-    position?: [number, number, number];
+function Planet({
+  radius,
+  segments = 64,
+  color = "blue",
+  position,
+  rotation,
+}: {
+  radius: number;
+  segments?: number;
+  color?: string;
+  position?: [number, number, number];
+  rotation?: [number, number, number];
+}) {
+  const meshRef = useRef<THREE.Mesh | null>(null);
 
-}
-
-function Planet(props: PlanetProps) {
-    const meshRef = useRef<THREE.Mesh | null>(null);
-
-    // Extract parameters from props or set default values
-    const {
-        radius = 3,
-        segments = 32,
-        color = "blue",
-        position = [0,0,0],
-        rotation = [0,0,0]
-    } = props;
-
-    return (
-        <mesh ref={meshRef} {...props}>
-            <sphereGeometry args={[props.radius, props.segments, props.segments]} />
-            <meshStandardMaterial color={color} />
-        </mesh>
-    );
+  return (
+    <mesh ref={meshRef} position={position} rotation={rotation}>
+      <sphereGeometry args={[radius, segments, segments]} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
 }
 
 export default Planet;
