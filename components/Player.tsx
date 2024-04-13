d3962a6d83e0464233c4ea5c022946209b2fe5c3
z07151th <thomas.hewitt-3@student.manchester.ac.uk>
2024-04-13 22:28:12 +0100

    animated astronaut

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,22 +1,16 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { forwardRef } from "react";
-import { Group, Object3D, Vector3 } from "three";
+import { Group, Vector3 } from "three";
+import { Astronaut } from "./Astronaut";
 
 export const Player = forwardRef<Group | null>(function Player(props, ref) {
   return (
     <group ref={ref}>
-      <mesh>
-        <sphereGeometry args={[1, 32, 32]} />
-        <meshStandardMaterial color="hotpink" />
-      </mesh>
-      <mesh position={[0, 0, 1]}>
-        <boxGeometry args={[0.8, 0.5, 0.8]} />
-        <meshStandardMaterial color="red" />
-      </mesh>
+      <Astronaut action="idle" />
 
-      <arrowHelper
+      {/* <arrowHelper
         args={[new Vector3(0, 1, 0), new Vector3(0, 0, 0), 5, 0xff0000, 3, 3]}
-      />
+      /> */}
       <PerspectiveCamera position={[0, 1, 10]} makeDefault={true} />
     </group>
   );
