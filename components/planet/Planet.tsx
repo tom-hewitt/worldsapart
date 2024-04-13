f07062af8654fa26971c16bf2248d392f9d7571d
ioan.gwenter <ioan.gwenter@student.manchester.ac.uk>
2024-04-13 14:36:09 +0100

    Add rotation

--- components/planet/Planet.tsx
+++ components/planet/Planet.tsx
@@ -1,22 +1,35 @@
 import * as THREE from "three";
-import React, {useRef} from "react";
+import React, { useRef } from "react";
+import { ThreeElements } from "@react-three/fiber";
 
-function Planet(props : any) {
-  const meshRef = useRef();
+interface PlanetProps {
+    name?: string;
+    radius?: number;
+    segments?: number;
+    color?: string;
+    rotation?: [number, number, number];
+    position?: [number, number, number];
 
-  // Extract parameters from props or set default values
-  const {
-    radius = 1,
-    segments = 32,
-    color = "orange",
-  } = props;
+}
 
-  return (
-    <mesh ref={meshRef} {...props}>
-      <sphereGeometry args={[radius, segments, segments]} />
-      <meshStandardMaterial color={color} />
-    </mesh>
-  );
+function Planet(props: PlanetProps) {
+    const meshRef = useRef<THREE.Mesh | null>(null);
+
+    // Extract parameters from props or set default values
+    const {
+        radius = 3,
+        segments = 32,
+        color = "blue",
+        position = [0,0,0],
+        rotation = [0,0,0]
+    } = props;
+
+    return (
+        <mesh ref={meshRef} {...props}>
+            <sphereGeometry args={[props.radius, props.segments, props.segments]} />
+            <meshStandardMaterial color={color} />
+        </mesh>
+    );
 }
 
 export default Planet;
